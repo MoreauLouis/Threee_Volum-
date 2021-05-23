0b88af890fe2bcfe19d06f530fa9a1492bb42d59
MoreauLouis <57181774+MoreauLouis@users.noreply.github.com>
2021-05-23 18:40:54 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,120 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { Mesh } from "three";
+
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const geometry = new THREE.ConeGeometry(0.8, 1.4, 3, 1, 1, true);
+
+// Materials
+
+const material = new THREE.LineBasicMaterial({
+  color: 0xe1bd23,
+  linewidth: 5,
+});
+// material.wireframe = true;
+
+// material.color = new THREE.Color(0xe1bd23);
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material);
+Mesh.color = new THREE.Color(0xff0000);
+scene.add(sphere);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 2);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true, //Background 3D
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+
+// const updateSphere = (event) => {
+//   sphere.position.y = window.scrollY * 0.001;
+// };
+// window.addEventListener("scroll", updateShpere);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.5 * elapsedTime;
+
+  // Update Orbital Controls
+  // controls.update()
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
